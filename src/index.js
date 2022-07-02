75c18eb1d868c7a766e8fd7aafb3addcca7847bb
lelderbe <fvdc@ya.ru>
2022-07-02 17:50:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 0;
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    while (n) {
+        result = result * 10 + (n % 10);
+        n = (n / 10) | 0;
+    }
+
+    return result;
+
+    /*
+    // Solution 2:
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    return String(n).split("").reverse().join("");
+    */
+};
