8559b4d0e81ed2a5a68286006e644a6692a1d111
lelderbe <fvdc@ya.ru>
2023-07-31 20:28:23 +0300

    refactor: update solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 module.exports = function reverse(n) {
-    let result = 0;
+    return String(Math.abs(n)).split('').reverse().join('');
 
-    if (n < 0) {
-        n = -n;
-    }
+    // or Algorithmic solution
+    // let result = 0;
 
-    while (n) {
-        result = result * 10 + (n % 10);
-        n = (n / 10) | 0;
-    }
+    // n = n < 0 ? -n : n;
 
-    return result;
+    // while (n) {
+    //     result = result * 10 + (n % 10);
+    //     n = Math.trunc(n / 10);
+    // }
 
-    /*
-    // Solution 2:
-
-    if (n < 0) {
-        n = -n;
-    }
-
-    return String(n).split("").reverse().join("");
-    */
+    // return result;
 };
